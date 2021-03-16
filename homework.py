c9c732fa095b836a0b3a7ed93ff80dee555eea1e
DenisSivko <densivko2306@yandex.ru>
2021-03-16 18:15:50 +0300

    Third commit: Code correction

--- homework.py
+++ homework.py
@@ -1,16 +1,18 @@
 import datetime as dt
-from typing import Any, List, Union
+from typing import List, Union
 
 
 class Record:
-    '''Конструктор класса, который используется для инициализации переменных.
-    amount - (денежная сумма или количество килокалорий),
-    comment - (комментарий поясняющий,
-    на что потрачены деньги или откуда взялись калории),
-    date - (дата передаётся в явном виде в конструктор,
-    либо присваивается значение по умолчанию — текущая дата).'''
     def __init__(self, amount: Union[int, float],
                  comment: str, date: str = None) -> None:
+        """
+        Инициализирует переменные.
+        amount - (денежная сумма или количество килокалорий),
+        comment - (комментарий поясняющий,
+        на что потрачены деньги или откуда взялись калории),
+        date - (дата передаётся в явном виде в конструктор,
+        либо присваивается значение по умолчанию — текущая дата).
+        """
         self.amount = amount
         self.comment = comment
         date_format = '%d.%m.%Y'
@@ -21,44 +23,45 @@ class Record:
 
 
 class Calculator:
-    '''Конструктор класса,
-    который используется для инициализации переменных.
-    limit - (дневной лимит трат/калорий, который задал пользователь)
-    records - (пустой список, в котором хранятся записи).'''
     def __init__(self, limit: Union[int, float]) -> None:
-        self.records: List[Any] = []
+        """
+        Инициализирует переменные.
+        limit - (дневной лимит трат/калорий, который задал пользователь)
+        records - (пустой список, в котором хранятся записи).
+        """
+        self.records: List[Record] = []
         self.limit = limit
 
-    '''Этот метод сохраняет новую запись о расходах/приёме пищи.'''
     def add_record(self, obj: Record) -> None:
+        """Сохраняет новую запись о расходах/приёме пищи."""
         self.records.append(obj)
 
-    '''Этот метод считает,
-    сколько было денег потрачено/калорий съедено сегодня.'''
     def get_today_stats(self) -> Union[int, float]:
+        """Считает, сколько было денег потрачено/калорий съедено сегодня."""
         today = dt.date.today()
-        counter_today: Union[int, float] = 0
-        for i in self.records:
-            if i.date == today:
-                counter_today = sum([i.amount], counter_today)
-        return counter_today
+        return sum(record.amount for record in self.records
+                   if record.date == today)
 
-    '''Этот метод считает,
-    сколько денег потрачено/калорий получено за последние 7 дней.'''
     def get_week_stats(self) -> Union[int, float]:
+        """
+        Считает, сколько денег потрачено/калорий получено за последние 7 дней.
+        """
         week = dt.timedelta(weeks=1)
-        counter_week: Union[int, float] = 0
-        for i in self.records:
-            if (dt.date.today() >= i.date > (dt.date.today() - week)):
-                counter_week = sum([i.amount], counter_week)
-        return counter_week
+        today = dt.date.today()
+        week_ago = today - week
+        return sum(record.amount for record in self.records
+                   if today >= record.date > week_ago)
+
+    def get_remains(self) -> Union[int, float]:
+        """Вычисляет остаток денег/калорий за сегодня."""
+        return self.limit - self.get_today_stats()
 
 
 class CaloriesCalculator(Calculator):
-    '''Этот метод вычисляет, сколько калорий вы можете съесть сегодня.'''
     def get_calories_remained(self) -> str:
-        if self.get_today_stats() < self.limit:
-            remains_calories = self.limit - self.get_today_stats()
+        """Вычисляет, сколько калорий вы можете съесть сегодня."""
+        remains_calories = super().get_remains()
+        if remains_calories > 0:
             return ('Сегодня можно съесть что-нибудь ещё, '
                     'но с общей калорийностью не более '
                     f'{remains_calories} кКал')
@@ -70,21 +73,26 @@ class CashCalculator(Calculator):
     EURO_RATE: float = 87.98
     RUB_RATE: float = 1
 
-    '''Этот метод вычисляет, сколько денег осталось на сегодняшний день.'''
     def get_today_cash_remained(self, currency: str) -> str:
+        """Вычисляет, сколько денег осталось на сегодняшний день."""
         currency_info = {
             'rub': ('руб', self.RUB_RATE),
             'usd': ('USD', self.USD_RATE),
             'eur': ('Euro', self.EURO_RATE),
         }
 
-        limit_rate = (abs(self.limit - self.get_today_stats()))
-        currency_name, currency_rate = currency_info[currency]
-        remains_cash = round(limit_rate / currency_rate, 2)
+        if currency not in currency_info:
+            raise ValueError('Данной валюты нет в словаре')
 
-        if self.get_today_stats() < self.limit:
+        limit_rate = super().get_remains()
+        if limit_rate == 0:
+            return 'Денег нет, держись'
+
+        currency_name, currency_rate = currency_info[currency]
+        remains_cash = round(abs(limit_rate) / currency_rate, 2)
+
+        if limit_rate > 0:
             return f'На сегодня осталось {remains_cash} {currency_name}'
-        elif self.get_today_stats() > self.limit:
+        else:
             return ('Денег нет, держись: '
                     f'твой долг - {remains_cash} {currency_name}')
-        return 'Денег нет, держись'
